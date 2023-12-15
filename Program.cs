18e34fadddb22e59f81bab3814b5de04daa0add7
sfilipt <filip.strand@hotmail.com>
2023-12-15 14:10:56 +0100

    Justerat konsoloutputs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,34 @@
 ﻿using System.Reflection;
 using System;
 
+
 namespace Laboration4
 {
+    
     internal class Program
     {
         static void Main(string[] args)
         {
+        //Jag använder variabeln personCounter för att hela tiden kunna hålla koll på hur många personer som lagts till i listan
+        //som håller information så att varje person får en tom plats i listan när den läggs till. 
+        //Utöver personCounter så deklarerar och tilldelar jag variabeln menuChoice som programmet kommer
+        //använda för att hantera användarens input när användaren står i menyn.
+        //Listan "Persons" skapas också här och jag anger att den ska lagra objekt av klassen "Person".
             int menuChoice=0;
             int personCounter = 0;
             List<Person> Persons = new List<Person>();
             while (menuChoice != 3)
             {
-                Console.WriteLine("\n Vad vill du göra nu? \n 1. Lägga till personer i listan.  " +
-                    "\n 2. Skriva ut alla personer som lagts till i listan. " +
-                    "\n 3. Avsluta programmet. ");
+                Console.WriteLine("\n------------------------------------------------------------" +
+                    "\n|    Vad vill du göra nu?                                  |" +
+                    "\n|    1. Lägga till personer i listan.                      |" +
+                    "\n|    2. Skriva ut alla personer som lagts till i listan.   |" +
+                    "\n|    3. Avsluta programmet.                                |" +
+                    "\n------------------------------------------------------------");
              string menuChoiceInput=Console.ReadLine();
                 while (!int.TryParse(menuChoiceInput, out menuChoice) || menuChoice > 3 || menuChoice < 1)
                 {
-                    Console.WriteLine("Skriv in en siffra mellan 1 och 3");
+                    Console.WriteLine("\n Felaktigt val: Skriv in en siffra mellan 1 och 3 \n");
                     menuChoiceInput = Console.ReadLine();
                 }
                 switch (menuChoice)
@@ -33,6 +43,9 @@ namespace Laboration4
 
                 }
             }
+            //I denna metod frågar programmet användaren efter input för att lagra värden i de properties som Person-klassen har:
+            //name, eyeColor, birthDate, gender och personHair. Sedan skapas ett nytt objekt av klassen Person där dessa värden lagras, 
+            //samtidigt läggs detta objekt i listan Persons
             void AddPerson()
             {
                 //Här initialiserar jag de variabler som behöver användas senare i metoden
@@ -40,7 +53,7 @@ namespace Laboration4
                 int birthMonth;
                 int birthDay;
                 int genderChoice;
-                Console.WriteLine("Skriv in namnet på personen");
+                Console.WriteLine("\nSkriv in namnet på personen: \n");
                 string name = Console.ReadLine();
                 //För varje input från användaren har jag skapat en while-loop som kommer loopa och uppmana användaren om korrekt input  
                 //tills programmet får det.
@@ -49,36 +62,36 @@ namespace Laboration4
                     Console.WriteLine("Du kan inte lämna detta fält tomt. Försök igen.");
                     name = Console.ReadLine();
                 }
-                Console.WriteLine($"Ange {name}s ögonfärg");
+                Console.WriteLine($"\nAnge {name}s ögonfärg: \n");
                 string eyeColor = Console.ReadLine();
                 while (string.IsNullOrWhiteSpace(eyeColor))
                 {
-                    Console.WriteLine("Du kan inte lämna detta fält tomt.");
+                    Console.WriteLine("Du kan inte lämna detta fält tomt. Försök igen");
                     eyeColor = Console.ReadLine();
                 }
-                Console.WriteLine($"Ange {name}s födelseår, fyra siffror.");
+                Console.WriteLine($"\nAnge {name}s födelseår, fyra siffror: \n");
                 string birthInputYear = Console.ReadLine();
                 while (!int.TryParse(birthInputYear, out birthYear) || int.Parse(birthInputYear) > 2023 || int.Parse(birthInputYear) < 1900)
                 {
                     Console.WriteLine("Du måste skriva in en giltig siffra mellan 1900 och 2023, och detta fält kan inte lämnas tomt.");
                     birthInputYear = Console.ReadLine();
                 }
-                Console.WriteLine($"Ange {name}s födelesmånad, siffra mellan 1 och 12");
+                Console.WriteLine($"\nAnge {name}s födelesmånad med en siffra mellan 1 och 12\n");
                 string birthInputMonth = Console.ReadLine();
                 while (!int.TryParse(birthInputMonth, out birthMonth) || int.Parse(birthInputMonth) > 12 || int.Parse(birthInputMonth) < 1)
                 {
-                    Console.WriteLine("Du måste skriva in en giltig siffra mellan 1 och 12, och detta fält kan inte lämnas tomt");
+                    Console.WriteLine("Du måste skriva in en siffra mellan 1 och 12, och detta fält kan inte lämnas tomt");
                     birthInputMonth= Console.ReadLine();
                 }
-                Console.WriteLine($"Ange {name}s födelesdag");
+                Console.WriteLine($"\nAnge {name}s födelesdag:\n");
                 string birthInputDay = Console.ReadLine();
                 while (!int.TryParse(birthInputDay, out birthDay) || int.Parse(birthInputDay) > 31 || int.Parse(birthInputDay) < 1)
                 {
-                    Console.WriteLine("Du måste skriva in en giltig siffra mellan 1 och 31, och detta fält kan inte lämnas tomt");
+                    Console.WriteLine("Du måste skriva in ett tal mellan 1 och 31, och detta fält kan inte lämnas tomt");
                     birthInputDay= Console.ReadLine();
                 }
                 DateOnly birthDate = new DateOnly(birthYear, birthMonth, birthDay);
-                Console.WriteLine($"Ange {name}s kön: Kvinna= 0, Man= 1 eller Annat= 2");
+                Console.WriteLine($"\nAnge {name}s kön: Kvinna= 0, Man= 1 eller Annat= 2\n");
                 string genderInputChoice = Console.ReadLine();
                 while (!int.TryParse(genderInputChoice, out genderChoice) || int.Parse(genderInputChoice) > 2 || int.Parse(genderInputChoice) < 0)
                 {
@@ -86,14 +99,14 @@ namespace Laboration4
                     genderInputChoice= Console.ReadLine();
                 }
                 Gender personGender = (Gender)genderChoice;
-                Console.WriteLine($"Skriv in hårlängd på {name}");
+                Console.WriteLine($"\nSkriv in hårlängd på {name}\n");
                 string hairLength = Console.ReadLine();
                 while (string.IsNullOrWhiteSpace(hairLength))
                 {
                     Console.WriteLine("Detta fält kan inte lämnas tomt. Försök igen.");
                     hairLength = Console.ReadLine();
                 }
-                Console.WriteLine($"Skriv in hårfärg på {name}");
+                Console.WriteLine($"\nSkriv in hårfärg på {name}\n");
                 string hairColor = Console.ReadLine();
                 while (string.IsNullOrWhiteSpace(hairColor))
                 {
@@ -102,7 +115,7 @@ namespace Laboration4
                 }
                 Hair personHair = new Hair(hairLength, hairColor);
                 //Programmet använder nu den validerade inputen för att skapa ett nytt objekt av klassen "Person" och lägga till det i listan
-                //"Persons. För att hålla koll på vilken plats i listan som personen ska läggas till i använder jag variabeln personCounter.
+                //"Persons. 
                 //För varje gång metoden AddPerson anropas och fullföljs av användaren så kommer variabeln personCounter att öka med 1.
                 Persons.Insert(personCounter, new Person(name, eyeColor, birthDate, personHair, personGender));
                 personCounter = personCounter++;
